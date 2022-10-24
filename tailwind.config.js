c70605c129537a7bcce8e58bfccaea53a6a97edc
Mads Nedergaard <nedergaardmads@gmail.com>
2022-10-24 07:22:18 +0200

    Adds tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            animation: {
+                'rainbow-text': 'rainbow-text 3s ease infinite',
+            },
+            keyframes: {
+                'rainbow-text': {
+                    '0%, 100%': {
+                        'background-size': '200% 200%',
+                        'background-position': 'left center',
+                    },
+                    '50%': {
+                        'background-size': '200% 200%',
+                        'background-position': 'right center',
+                    },
+                },
+            },
+        },
+    },
+    plugins: [require('@tailwindcss/typography')],
+};
