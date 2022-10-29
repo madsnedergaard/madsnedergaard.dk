3d16def8f17fa5451064af3e48e40a2d49671b11
Mads Nedergaard <nedergaardmads@gmail.com>
2022-10-29 23:25:26 +0200

    Fixes NavLink for real

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -5,21 +5,22 @@ import React from 'react';
 type NavLinkProps = {
   children: string;
   href: string;
-  [x: string]: any;
+  className?: string;
 };
-export default function NavLink({ children, className, ...props }: NavLinkProps) {
-  const isExternal = props.href.startsWith('http');
-  const Component = isExternal ? 'a' : Link;
+export default function NavLink({ children, href, className = '' }: NavLinkProps) {
+  const isExternal = href.startsWith('http');
+  const ComponentWrapper = isExternal ? React.Fragment : Link;
 
   return (
-    <Component
-      {...props}
-      className={classNames(
-        'border-b-2 border-zinc-500 text-zinc-500 transition-all hover:border-zinc-800 hover:text-zinc-800 dark:hover:border-zinc-400 dark:hover:text-zinc-400',
-        className
-      )}
-    >
-      {children}
-    </Component>
+    <ComponentWrapper href={href}>
+      <a
+        className={classNames(
+          'border-b-2 border-zinc-500 text-zinc-500 transition-all hover:border-zinc-800 hover:text-zinc-800 dark:hover:border-zinc-400 dark:hover:text-zinc-400',
+          className
+        )}
+      >
+        {children}
+      </a>
+    </ComponentWrapper>
   );
 }
