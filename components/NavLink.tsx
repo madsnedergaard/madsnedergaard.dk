1c6d7bfe591b1e0c9c6f133ab3efd3cc39a2873f
Mads Nedergaard <nedergaardmads@gmail.com>
2022-10-29 23:16:29 +0200

    Fixes link in header

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import Link from 'next/link';
 import React from 'react';
 
 type NavLinkProps = {
@@ -7,15 +8,18 @@ type NavLinkProps = {
   [x: string]: any;
 };
 export default function NavLink({ children, className, ...props }: NavLinkProps) {
+  const isExternal = props.href.startsWith('http');
+  const Component = isExternal ? 'a' : Link;
+
   return (
-    <a
+    <Component
+      {...props}
       className={classNames(
         'border-b-2 border-zinc-500 text-zinc-500 transition-all hover:border-zinc-800 hover:text-zinc-800 dark:hover:border-zinc-400 dark:hover:text-zinc-400',
         className
       )}
-      {...props}
     >
       {children}
-    </a>
+    </Component>
   );
 }
