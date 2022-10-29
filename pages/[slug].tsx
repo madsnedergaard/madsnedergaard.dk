7bd2ee47e1164854a13c6dd2542020a3d2ca66b5
Mads Nedergaard <nedergaardmads@gmail.com>
2022-10-29 23:06:16 +0200

    Adds mdx-based posts

---
+++ pages/[slug].tsx
@@ -0,0 +1,71 @@
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { getPost, getPosts } from '../utils/posts';
+import type { PostData } from '../utils/posts';
+import Header from '../components/Header';
+import dynamic from 'next/dynamic';
+
+const components = {
+  Highlight: dynamic(() => import('../components/Highlight')),
+};
+
+const IntroSection = ({ data }: { data: PostData }) => (
+  <div className="text-center">
+    <h1 className="m-4 mb-4 text-5xl font-bold sm:mt-12 sm:text-7xl">{data.title}</h1>
+    <div className="text-md mt-2 flex flex-col items-center justify-center space-x-2 sm:flex-row sm:text-sm">
+      <time className=" italic text-zinc-500">Updated {data.updatedAt}</time>
+      <span className="hidden text-zinc-500 sm:block">&mdash;</span>
+      <div className="flex flex-wrap justify-center space-x-1">
+        {data?.tags?.map((t: string) => (
+          <span key={t}>
+            <span className="mr-[1px]">#</span>
+            {t}
+          </span>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+interface PostProps {
+  data: PostData;
+  content: MDXRemoteSerializeResult;
+}
+export default function Post({ data, content }: PostProps) {
+  return (
+    <div>
+      <Header />
+      <article className="container prose prose-zinc mx-auto pb-8 dark:prose-invert">
+        <IntroSection data={data} />
+        <hr className="mx-auto my-8 w-80 border-zinc-200 dark:border-zinc-600" />
+        <MDXRemote {...content} components={components} />
+      </article>
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const posts = await getPosts();
+  const paths = posts.map((post) => ({ params: { slug: post.slug } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+interface StaticProps {
+  params: {
+    slug: string;
+  };
+}
+export const getStaticProps = async ({ params }: StaticProps) => {
+  const post = await getPost(params.slug);
+  const mdxSource = await serialize(post.content);
+
+  return {
+    props: {
+      data: post.data,
+      content: mdxSource,
+    },
+  };
+};
---
+++ utils/posts.ts
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+export interface PostData {
+  title: string;
+  updatedAt: string;
+  createdAt: string;
+  tags?: string[];
+}
+
+interface Post {
+  data: PostData;
+}
+interface PostWithContent extends Post {
+  content: string;
+}
+export interface PostWithSlug extends Post {
+  slug: string;
+}
+
+export const getPost = (slug: string): PostWithContent => {
+  const fileContents = fs.readFileSync(path.join(`posts/${slug}.mdx`), 'utf8');
+  const { data, content } = matter(fileContents);
+  return {
+    data: data as PostData,
+    content,
+  };
+};
+
+export const getPosts = (): PostWithSlug[] => {
+  const files = fs.readdirSync(path.join('posts'));
+  const allPostsData = files.map((fileName) => {
+    const slug = fileName.replace('.mdx', '');
+    const fileContents = fs.readFileSync(path.join(`posts/${slug}.mdx`), 'utf8');
+    const { data } = matter(fileContents);
+    return {
+      slug,
+      data: data as PostData,
+    };
+  });
+
+  return allPostsData;
+};
