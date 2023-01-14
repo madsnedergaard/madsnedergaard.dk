bf2eb411dbfbf77a3930cc2ef6c6bcd6b671bd91
Mads Nedergaard <nedergaardmads@gmail.com>
2023-01-14 23:59:44 +0100

    Filters out non-mdx files

--- utils/posts.ts
+++ utils/posts.ts
@@ -30,15 +30,17 @@ export const getPost = (slug: string): PostWithContent => {
 
 export const getPosts = (): PostWithSlug[] => {
   const files = fs.readdirSync(path.join('posts'));
-  const allPostsData = files.map((fileName) => {
-    const slug = fileName.replace('.mdx', '');
-    const fileContents = fs.readFileSync(path.join(`posts/${slug}.mdx`), 'utf8');
-    const { data } = matter(fileContents);
-    return {
-      slug,
-      data: data as PostData,
-    };
-  });
+  const allPostsData = files
+    .filter((file) => path.extname(file) === '.mdx')
+    .map((fileName) => {
+      const slug = fileName.replace('.mdx', '');
+      const fileContents = fs.readFileSync(path.join(`posts/${slug}.mdx`), 'utf8');
+      const { data } = matter(fileContents);
+      return {
+        slug,
+        data: data as PostData,
+      };
+    });
 
   return allPostsData;
 };
