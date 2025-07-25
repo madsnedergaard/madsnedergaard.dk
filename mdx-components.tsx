0dac5e2e8e6b1868a14b7e94e21a1685b771290c
madsnedergaard <nedergaardmads@gmail.com>
2025-07-25 22:41:51 +0200

    Updates components and styling

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -7,6 +7,14 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h2: ({ children }) => <h2 className="mb-3 text-2xl font-semibold">{children}</h2>,
     h3: ({ children }) => <h3 className="mb-2 text-xl font-semibold">{children}</h3>,
     p: ({ children }) => <p className="mb-4">{children}</p>,
+    a: ({ children, href }) => (
+      <a
+        href={href}
+        className="hover:text-primary underline-offset-2 transition-all hover:underline-offset-4"
+      >
+        {children}
+      </a>
+    ),
     img: (props) => (
       <Image sizes="100vw" style={{ width: '100%', height: 'auto' }} {...(props as ImageProps)} />
     ),
