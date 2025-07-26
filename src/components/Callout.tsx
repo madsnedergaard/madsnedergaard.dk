2d8ea3fe72be93e31d52c742beda2ec4bc930eda
madsnedergaard <nedergaardmads@gmail.com>
2025-07-26 23:45:44 +0200

    Reworks component to ensure colors work

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -1,31 +1,24 @@
 import { BookOpenTextIcon } from 'lucide-react';
 import { cn } from '@/utils/cn';
 
-type CalloutType = 'tldr' | 'info' | 'warning';
-
-const CalloutTypes = {
-  tldr: {
-    title: 'tl;dr',
-    icon: <BookOpenTextIcon size={18} strokeWidth={2} />,
-    color: 'primary',
-  },
-  info: {
-    title: 'Info',
-    icon: <BookOpenTextIcon size={18} strokeWidth={2} />,
-    color: 'bg-blue-500',
-  },
-  warning: {
-    title: 'Warning',
-    icon: <BookOpenTextIcon size={18} strokeWidth={2} />,
-    color: 'bg-amber-500',
-  },
-};
-
-export const Callout = ({ type, children }: { type: CalloutType; children: React.ReactNode }) => {
-  const { title, icon, color } = CalloutTypes[type];
-
+const Callout = ({
+  title,
+  icon,
+  color,
+  children,
+}: {
+  title: string;
+  icon: React.ReactNode;
+  color: string;
+  children: React.ReactNode;
+}) => {
   return (
-    <div className={cn('border-l-4 px-6 py-2', `border-${color}`)}>
+    <div
+      className={cn(
+        'mx-6 border-b-2 py-2 sm:mx-0 sm:border-b-0 sm:border-l-4 sm:px-6',
+        `border-${color}`
+      )}
+    >
       <div
         className={cn(
           '-mt-2 mb-2 flex -rotate-2 items-center gap-1 pb-2 text-sm font-bold',
@@ -34,7 +27,13 @@ export const Callout = ({ type, children }: { type: CalloutType; children: React
       >
         {icon} {title}
       </div>
-      <div className="text-lg text-zinc-600 dark:text-zinc-400 [&>p]:m-0">{children}</div>
+      <div className="text-zinc-600 sm:text-lg dark:text-zinc-400 [&>p]:m-0">{children}</div>
     </div>
   );
 };
+
+export const TLDR = ({ children }: { children: React.ReactNode }) => (
+  <Callout title="tl;dr" icon={<BookOpenTextIcon size={18} strokeWidth={2} />} color="primary">
+    {children}
+  </Callout>
+);
