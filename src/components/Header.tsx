100000d6125bf45b62d7217a4869430eb022b0ab
madsnedergaard <nedergaardmads@gmail.com>
2025-09-25 20:58:18 +0200

    Updates header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,14 @@
-import NavLink from './NavLink';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="mb-0 max-w-screen-md p-6 sm:mx-auto">
-      <header className="flex flex-row items-center justify-between">
-        <p className="animate-rainbow-text bg-gradient-to-r from-[#3494E6] via-[#5961DF] to-[#EC6EAD] bg-clip-text text-transparent italic dark:via-[#fcff9e] dark:to-[#EC6EAD]">
-          Thoughts of Mads Nedergaard
-        </p>
-        <NavLink href="/" className="text-sm">
-          Home
-        </NavLink>
+    <div className="mb-0 w-full max-w-screen-md p-6 sm:mx-auto">
+      <header className="flex flex-row items-center justify-center">
+        <Link href="/">
+          <p className="animate-rainbow-text bg-gradient-to-r from-[#3494E6] via-[#5961DF] to-[#EC6EAD] bg-clip-text text-transparent italic dark:via-[#fcff9e] dark:to-[#EC6EAD]">
+            Thoughts of Mads Nedergaard
+          </p>
+        </Link>
       </header>
     </div>
   );
