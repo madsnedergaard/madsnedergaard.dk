0d7262f86f95d357c6c216ca48303eacc09e8959
madsnedergaard <nedergaardmads@gmail.com>
2025-10-08 22:42:39 +0200

    Uses AI badge too

--- src/components/PostIntro.tsx
+++ src/components/PostIntro.tsx
@@ -1,27 +1,27 @@
 import { PostMeta } from '../utils/posts';
+import { AIBadge } from './AIBadge';
 
 export const PostIntro = ({ data }: { data: PostMeta }) => (
-  <div className="text-center">
+  <div className="text-center sm:mt-12">
+    <AIBadge />
     <h1
-      className="m-4 mb-3 text-3xl font-bold sm:mt-12 sm:text-4xl"
+      className="m-4 mb-3 text-3xl font-bold sm:text-4xl"
       dangerouslySetInnerHTML={{ __html: data.title.replace('\\n', '<br />') }}
     ></h1>
-    <p className="text-zinc-500 italic">{data.updatedAt || data.createdAt}</p>
+
     <div>
-      <div className="text-md mt-2 flex flex-col items-center justify-center space-x-2 sm:flex-row sm:text-sm">
-        {data.readingTime} min read
+      <div className="text-md mt-2 flex flex-col items-center justify-center space-x-4 text-zinc-400 sm:flex-row sm:text-sm dark:text-zinc-500">
+        <span>{data.readingTime} min read</span>
+        <span>{data.updatedAt || data.createdAt}</span>
         {data.tags && data.tags.length > 0 && (
-          <>
-            <span className="ml-2 hidden text-zinc-500 sm:block">&mdash;</span>
-            <div className="flex flex-wrap justify-center space-x-1 text-zinc-500 dark:text-zinc-400">
-              {data.tags?.map((t: string) => (
-                <span key={t}>
-                  <span className="mr-[1px] text-zinc-400 dark:text-zinc-500">#</span>
-                  {t}
-                </span>
-              ))}
-            </div>
-          </>
+          <div className="flex flex-wrap justify-center space-x-1">
+            {data.tags?.map((t: string) => (
+              <span key={t}>
+                <span className="mr-[1px] text-zinc-300 dark:text-zinc-600">#</span>
+                {t}
+              </span>
+            ))}
+          </div>
         )}
       </div>
     </div>
